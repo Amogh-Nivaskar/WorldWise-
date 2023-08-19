aebe3a1969a018f685a0c380a05526f3f447253f
SoulLess999 <amoghpnivas@gmail.com>
2023-08-19 16:46:06 +0530

    installing necessary packages

---
+++ server/app.js
@@ -0,0 +1,14 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const path = require("path");
+const methodOverride = require("method-override");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(methodOverride("_method"));
